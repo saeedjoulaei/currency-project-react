98ac4beaeecb4dedd6cea7e734ac58c2a0933ccd
saeed joulaei <saeedjoulaei86@gmail.com>
2023-09-27 11:56:48 +0330

    api.js created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import './App.css';
-
-// Components
-import Landing from './components/Landing';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Landing />
-    </div>
-  );
+  return <div className="App">app</div>;
 }
 
 export default App;
